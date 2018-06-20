976a3c49d4af1aad0d597a6c7dcc51850ff8fec1
mymaizi <lqjlmp@gmail.com>
2018-06-20 10:52:21 +0800

    扩展发送服务器参数属性（data）

--- js/koext/kogrid.js
+++ js/koext/kogrid.js
@@ -9,14 +9,17 @@
              * @param {单元格宽度}  width 
              * @param {数据类型}  type 
              * @param {表头分组标识} columngroup
+             * 
+             * data 发送到服务器的外部参数(json)
              */
 
             var self = this; _options = $.extend({}, {
                 width: $(".content-wrapper").width(), datatype: "json", columns: [], localdata: [], url: "", columnsresize: true,
                 selectionmode: "checkbox",
-                columngroups: [],
                 sortable: true,
-                pageable: true
+                pageable: true,
+                pagermode: 'simple',
+                data:{}
             }, options || {});
             //处理source-->datafields字段
             var _fields = [];
@@ -30,13 +33,21 @@
             var _source =
             {
                 datatype: _options.datatype,
-                datafields: _fields
+                datafields: _fields,
+                sort: function () {
+                    self.grid.jqxGrid('updatebounddata', 'sort');
+                },
+                processdata: function (data) {
+                    $.each(self.options.data,function(key,value){
+                        data[key]=value;
+                    });
+                }
             };
             //判断数据处理方式，一旦传入了url代表需要请求远程api，反之则使用本地数据展示
-            if (_options.localdata.length!=0) {
+            if (_options.localdata.length != 0) {
                 _source.localdata = _options.localdata;
-            }else if(_options.url != ""){
-                _source.url=_options.url;
+            } else if (_options.url != "") {
+                _source.url = _options.url;
             }
             //数据适配器
             var _dataAdapter = new $.jqx.dataAdapter(_source);
@@ -49,14 +60,32 @@
                 columns: _options.columns,
                 columngroups: _options.columngroups,
                 sortable: _options.sortable,
-                pageable: _options.pageable
+                pageable: _options.pageable,
+                pagermode: _options.pagermode
             }
             self.options = _gridOptions;
 
 
             ////////////////////////////////////////////////////华丽的分割，未完待续，以下是所有函数封装/////////////////////////////////////////////////////////////
+            /**
+            * 本地化
+            */
+            self.localizestrings = function () {
+                var localizationobj = {};
+                localizationobj.pagerrangestring = " 总条数 ";
+                localizationobj.pagernextbuttonstring = "下一页";
+                localizationobj.pagerpreviousbuttonstring = "上一页";
+                localizationobj.pagerlastbuttonstring = "尾页";
+                localizationobj.pagerfirstbuttonstring = "首页";
+                localizationobj.loadtext = "加载中...";
+                localizationobj.sortascendingstring = "降序排序";
+                localizationobj.sortdescendingstring = "升序排序";
+                localizationobj.sortremovestring = "删除排序";
+                localizationobj.emptydatastring = "没有数据显示";
+                self.grid.jqxGrid('localizestrings', localizationobj);
+            }
             self.refresh = function () {
-                self.grid.jqxGrid('updatebounddata', 'cells');
+                self.grid.jqxGrid('updatebounddata');
             }
             self.getselectdata = function () {
                 var indexs = self.grid.jqxGrid('getselectedrowindexes');
@@ -75,7 +104,6 @@
             self.exportdata = function (type = "xls") {
                 self.grid.jqxGrid('exportdata', type, "jqxGrid");
             }
-
         }
     }
     ko.bindingHandlers.koGrid = {
@@ -84,7 +112,14 @@
         },
         update: function (element, valueAccessor) {
             var value = valueAccessor();
+            /**
+             * 当element绑定完成设置本地化数据
+             */
+            $(element).on("bindingcomplete", function () {
+                value.localizestrings();
+            });
             value.grid = $(element).jqxGrid(value.options);
+
         }
     };
 })(jQuery, ko);
